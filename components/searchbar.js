d2f77a6a9d2783b725109655b8a98f07d0e48685
Abdurahman Sherif <DevOpt@users.noreply.github.com>
2023-07-18 00:12:27 -0400

    init gpt

--- components/searchbar.js
+++ components/searchbar.js
@@ -6,7 +6,7 @@ const SearchBar = () => {
   const [textInput, setTextInput] = useState('');
   const [resultInput, setResultInput] = useState('');
   const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
 
